1eef6ef3b581d1fae6ad95b9410f6fef8a49e32d
Ryan de Man <Ryan.m.de.man@gmail.com>
2023-03-27 13:52:18 +0200

    Toevoegen van images via javascript
    Javascript foto's toevoegen op basis van list en foto folder

--- js/main.js
+++ js/main.js
@@ -104,3 +104,40 @@ function registerServiceWorker() {
             });
     }
 }
+
+//Global variables
+let imageList = ['drempel', 'toilet', 'lepel'];
+let fotoField;
+
+/**
+ * Initialize after the DOM is ready
+ */
+function init() {
+    fotoField = document.getElementById("foto-field");
+    console.log(fotoField);
+
+    createFotoField();
+}
+
+/**
+ * Generate the playing field dynamically with all the available images
+ */
+function createFotoField() {
+    imageList = shuffleArray(imageList);
+
+    for (let i = 0; i < imageList.length; i++) {
+        let card = document.createElement('div');
+        card.setAttribute('class', 'foto-card');
+        fotoField.appendChild(card);
+
+        let title = document.createElement('h2');
+        card.appendChild(title);
+        let image = document.createElement('img');
+        image.src = `img/${imageList[i]}.png`;
+        card.appendChild(image);
+    }
+}
+
+function shuffleArray(array) {
+    return array;
+}
