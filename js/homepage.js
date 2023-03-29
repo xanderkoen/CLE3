020653654a9ab076653dbc36283265e71053dd9b
Xander <xkoen@hotmail.com>
2023-03-29 11:21:48 +0200

    homepage animaties

--- js/homepage.js
+++ js/homepage.js
@@ -1,14 +1,71 @@
 //voeg hier die coole code toe die alles gaat doen :)
-let hamburger = true
+let hamburger = false
+let tree = false
+let deur = false
 
-function change() {
+function hamburgerchange() {
 
 
     if (hamburger === false){
-        document.getElementById("placeholder").src = "./../media/placeholder2.png";
+        document.getElementById("deur").src = "./../media/deurdicht.png";
+        document.getElementById("tree").src = "./../media/treedicht.png";
         hamburger = true;
+        tree = false;
+        deur = false;
+        document.getElementById("hamburger").src = "../media/HamburgerGif.gif";
+        setTimeout(() => { document.getElementById("hamburger").src = "./../media/hamburgeropen.png"; }, 350);
+        document.getElementById("hamburger").onclick = "linkburger()";
+
     }else{
-        document.getElementById("placeholder").src = "./../media/placeholder.png";
+        document.getElementById("hamburger").src = "./../media/hamburgerdicht.png";
         hamburger = false;
+    }
+}
+
+function deurchange() {
+
+
+    if (deur === false){
+        document.getElementById("tree").src = "./../media/treedicht.png";
+        document.getElementById("hamburger").src = "./../media/hamburgerdicht.png";
+        deur = true;
+        hamburger = false;
+        tree = false;
+        document.getElementById("deur").src = "../media/DeurGif.gif";
+        setTimeout(() => { document.getElementById("deur").src = "./../media/deuropen.png"; }, 900);
+    }else{
+        document.getElementById("deur").src = "./../media/deurdicht.png";
+        deur = false;
+    }
+}
+
+function treechange() {
+
+
+    if (tree === false){
+        document.getElementById("hamburger").src = "./../media/hamburgerdicht.png";
+        document.getElementById("deur").src = "./../media/deurdicht.png";
+
+        tree = true;
+        hamburger = false;
+        deur = false;
+        document.getElementById("tree").src = "../media/BordGifLoop.gif";
+        setTimeout(() => { document.getElementById("tree").src = "./../media/treeopen.png"; }, 500);
+
+    }else{
+        document.getElementById("tree").src = "./../media/treedicht.png";
+        tree = false;
+    }
+
+    function linkburger() {
+        console.log("nig")
+    }
+
+    function linkdeur() {
+
+    }
+
+    function linktree() {
+
     }
 }
